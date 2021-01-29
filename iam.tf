547bbb65e89855f5d4e8d096840b0c41fc600cfd
Jesse Frederick <frederick.jesse@gmail.com>
2021-01-29 16:06:14 -0500

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -6,7 +6,7 @@ resource "aws_iam_group" "dms_users_group" {
 resource "aws_iam_policy" "dms_users_policy" {
   name        = "dms_users_policy"
   description = "Allows users full access to AWS Database Migration Service"
-  policy      = file("iam_policies/policydmsaccess.json")
+  policy      = file("policydmsaccess.json")
 }
 
 resource "aws_iam_group_policy_attachment" "dms_users_gpa" {
